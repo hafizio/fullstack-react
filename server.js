b8e6214079c7c5b8c994b67bb867e19e3acf7146
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-10-12 11:14:37 -0500

    simplify server code

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ var FalcorServer = require('falcor-express'),
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/model.json', FalcorServer.dataSourceRoute(() => new NamesRouter()))
 app.use(express.static('.'))
-app.listen(9090, (err) => {
+app.listen(9090, err => {
     if (err) {
         console.error(err)
         return
